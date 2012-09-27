ed5cbdabc32c20180b1fe835056d3625b95ec67d
Doronin Stanislav <overloop@mail.ru>
2012-09-27 11:44:14 +0400

    gpl ftw

--- insertremove/button.h
+++ insertremove/button.h
@@ -1,3 +1,22 @@
+/**
+   @author Doronin Stanislav <mugisbrows@gmail.com>
+
+   This file is part of InsertRemovePanel.
+
+   InsertRemovePanel is free software: you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or (at
+   your option) any later version.
+
+   InsertRemovePanel is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+   Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with InsertRemovePanel. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #ifndef INSERTREMOVE_BUTTON_H
 #define INSERTREMOVE_BUTTON_H
@@ -25,9 +44,15 @@ public:
 
 protected:
 
+    /**
+      QSize -> (int x, int y) or (int x, int y), QPoint -> (int x, int y) or (int x, int y), according to orientation
+     */
     virtual void getSizes(QTableView* table, QAbstractItemModel* model, int* m, int* n, int* btn1, int* btn2, int* pnt1, int* pnt2, int* ofs1, int* ofs2) const;
     virtual void nearestBorder(int policy, int pos, int sizes[], int count, int* n, int* coord) const;
     virtual void nearestMiddle(int policy, int pos, int sizes[], int count, int* n, int* coord) const;
+
+    /** Widget size minus scrolls sizes
+      */
     QSize usefulWidgetSize(QAbstractScrollArea* widget) const;
 
     InsertRemove::Type _type;
@@ -37,6 +62,7 @@ protected:
     PolicyFlags _policy;
 
 public slots:
+
     void placeButton();
     void setPoint(const QPoint& point);
     void setPolicy(PolicyFlags policy);
--- insertremove/insertremove_matrix.cpp
+++ insertremove/insertremove_matrix.cpp
@@ -1,3 +1,23 @@
+/**
+   @author Doronin Stanislav <mugisbrows@gmail.com>
+
+   This file is part of InsertRemovePanel.
+
+   InsertRemovePanel is free software: you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or (at
+   your option) any later version.
+
+   InsertRemovePanel is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+   Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with InsertRemovePanel. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include "insertremove/matrix.h"
 
 namespace InsertRemove {
